6a0a2a68984605d49b8eb94414160e4814e0f82c
ShreeanshBabu <shreeanshbabu194@gmail.com>
2026-05-15 23:14:52 +0530

    react

---
+++ React/my-second-app/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./ProductCard";
+
+function App(){
+  // Js Code
+
+
+  return(
+    // HTML
+    <div>
+
+      <h1>Hello React</h1>
+
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+
+
+    </div>
+  )
+}
+
+export default App
